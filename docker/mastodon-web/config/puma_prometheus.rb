27a7c242474347a6429e958566b2d06d78221885
Dominique Quatravaux <dominique@quatravaux.org>
2025-02-11 10:36:54 +0100

    [feature] Turn on ActiveRecord time series
    Apparently `type` is also used as a time series label for the
    `::Process` stuff, and thus we want to set it up for the
    `::ActiveRecord` stuff as well.

--- docker/mastodon-web/config/puma_prometheus.rb
+++ docker/mastodon-web/config/puma_prometheus.rb
@@ -1,5 +1,7 @@
 
 on_worker_boot do
   require 'prometheus_exporter/instrumentation'
-  PrometheusExporter::Instrumentation::Process.start(type:"web")
+  PrometheusExporter::Instrumentation::Process.start(type: "web")
+  PrometheusExporter::Instrumentation::ActiveRecord.start(
+    custom_labels: { type: "web" })
 end
