65f9923c24476c25246f1d2c5cd81cdeae9534df
Dominique Quatravaux <dominique@quatravaux.org>
2025-02-11 11:04:33 +0100

    [feature] Puma-specific instrumentation

--- docker/mastodon-web/config/puma_prometheus.rb
+++ docker/mastodon-web/config/puma_prometheus.rb
@@ -5,3 +5,10 @@ on_worker_boot do
   PrometheusExporter::Instrumentation::ActiveRecord.start(
     custom_labels: { type: "web" })
 end
+
+after_worker_boot do
+  require 'prometheus_exporter/instrumentation'
+  if !PrometheusExporter::Instrumentation::Puma.started?
+    PrometheusExporter::Instrumentation::Puma.start
+  end
+end
