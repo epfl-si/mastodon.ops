f668f75a92b73a3506f5a65c20d1661dbdee3898
Nicolas Borboën <ponsfrilus@gmail.com>
2025-04-07 22:52:15 +0200

    [feature] Add some new metrics
    - status_count_gauge
    - local_status_count_gauge
    - boost_count_gauge
    - local_boost_count_gauge
    - favourite_count_gauge
    - local_favourite_count_gauge

--- docker/mastodon-web/monitoring/adhoc.rb
+++ docker/mastodon-web/monitoring/adhoc.rb
@@ -10,8 +10,46 @@ class MastodonAdHocCollector < PrometheusExporter::Server::TypeCollector
   end
 
   def metrics
+    # Count users
     user_count_gauge = PrometheusExporter::Metric::Gauge.new('mastodon_user_count', 'Number of Mastodon users')
     user_count_gauge.observe User.count
-    [user_count_gauge]
+
+    # Count all statuses
+    status_count_gauge = PrometheusExporter::Metric::Gauge.new('mastodon_status_count', 'Number of Mastodon statuses')
+    status_count_gauge.observe Status.count
+
+    # Count local statuses
+    local_status_count_gauge = PrometheusExporter::Metric::Gauge.new('mastodon_local_status_count', 'Number of Mastodon local statuses')
+    local_status_count_gauge.observe Status.where(local: true).count
+
+    # Counting boosts (repost)
+    boost_count_gauge = PrometheusExporter::Metric::Gauge.new('mastodon_boost_count', 'Number of Mastodon boosts')
+    boost_count_gauge.observe Status.where.not(reblog_of_id: nil).count
+
+    # Counting boosts of local statuses
+    local_boost_count_gauge = PrometheusExporter::Metric::Gauge.new('mastodon_local_boost_count', 'Number of Mastodon local boosts')
+    local_boost_count_gauge.observe Status.joins('JOIN statuses as original_statuses ON statuses.reblog_of_id = original_statuses.id')
+                                          .where('original_statuses.local': true)
+                                          .count
+
+    # Counting favourites (likes)
+    favourite_count_gauge = PrometheusExporter::Metric::Gauge.new('mastodon_favourite_count', 'Number of Mastodon favourites')
+    favourite_count_gauge.observe Favourite.count
+
+    # Count local favourites
+    local_favourite_count_gauge = PrometheusExporter::Metric::Gauge.new('mastodon_local_favourite_count', 'Number of Mastodon local favourites')
+    local_favourite_count_gauge.observe Favourite.joins(:status)
+                                                 .where(statuses: { local: true })
+                                                 .count
+
+    [
+      user_count_gauge,
+      status_count_gauge,
+      local_status_count_gauge,
+      boost_count_gauge,
+      local_boost_count_gauge,
+      favourite_count_gauge,
+      local_favourite_count_gauge
+    ]
   end
 end
